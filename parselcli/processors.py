76e72ab2704815090b2f04cbaaf23ecc965a03de
Bernardas <bernardas.alisauskas@gmail.com>
2017-06-29 16:17:26 +0000

    add First processor

--- parselcli/processors.py
+++ parselcli/processors.py
@@ -1,10 +1,17 @@
-
 class Strip:
     def __call__(self, vs):
         if isinstance(vs, list):
             return [v.strip() for v in vs]
         return vs.strip()
 
-if __name__ == '__main__':
-    s = Strip()
-    print(type(s).__name__)
+
+class First:
+    """Take first element if list has only one value"""
+    def __call__(self, vs):
+        if isinstance(vs, list):
+            if len(vs) < 2:
+                return vs[0] if vs else ''
+            return vs
+        return vs or ''
+
+
