97c6e723ed40d4e2f5ac970ee75ebfca58ed4854
granitosaurus <tinarg@protonmail.com>
2019-05-01 11:32:44 +0000

    update lazy_dict_merge and it's tests

--- parselcli/utils.py
+++ parselcli/utils.py
@@ -1,4 +1,4 @@
-import collections
+from copy import deepcopy
 
 
 def lazy_dict_merge(root, update):
@@ -9,16 +9,16 @@ def lazy_dict_merge(root, update):
     i.e. update['foo'] will only be copied to root if it doesn't have 'foo' already
 
     >>> lazy_dict_merge({'foo': '1'}, {'bar': '2'})
-    {}
-    >>> lazy_dict_merge({'foo': '1'}, {'bar': '2'})
-    {}
+    {'foo': '1', 'bar': '2'}
+    >>> lazy_dict_merge({'foo': '1'}, {'bar': '2', 'foo': '3'})
+    {'foo': '1', 'bar': '2'}
     """
-    result = {}
-    for k, v in update.iteritems():
+    result = deepcopy(root)
+    for k, v in update.items():
         if k not in root:
             result[k] = v
             continue
-        if isinstance(root[k], dict) and isinstance(update[k], collections.Mapping):
+        if isinstance(root[k], dict) and isinstance(update[k], dict):
             result[k] = lazy_dict_merge(root[k], update[k])
             continue
         result[k] = root[k]
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,15 +3,23 @@ from parselcli.utils import lazy_dict_merge
 
 def test_lazy_dict_merge():
     expected = [
-        (
+        (  # two different ones
             {'foo': '1'}, {'bar': '2'},
             {'foo': '1', 'bar': '2'}
         ),
-        # (
-        #     {},
-        #     {}
-        # ),
+        (  # existing key
+            {'foo': '1'}, {'bar': '2', 'foo': '3'},
+            {'foo': '1', 'bar': '2'}
+        ),
+        (  # existing key
+            {'nested': {'foo': '1'}}, {'nested': {'bar': '2'}},
+            {'nested': {'foo': '1', 'bar': '2'}}
+        ),
+        (  # combined
+            {'nested': {'foo': '1'}}, {'nested': {'bar': '2'}, 'extra': '1'},
+            {'nested': {'foo': '1', 'bar': '2'}, 'extra': '1'}
+        ),
     ]
 
     for arg1, arg2, exp in expected:
-        assert lazy_dict_merge(arg1, arg2) == expected
+        assert exp == lazy_dict_merge(arg1, arg2)
